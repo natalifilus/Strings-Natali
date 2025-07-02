58c3c50b755a3782a0eb699ee2a12367c2556c72
natalifilus <natali.filus@escola.pr.gov.br>
2025-07-02 21:23:28 +0000

    mudando algumas coisas

--- script.js
+++ script.js
@@ -1,36 +1,58 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-function mostraPalavrasChave() { 
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
+
+
+
+function mostraPalavrasChave() {
+
   const texto = document.querySelector("#entrada-de-texto").value;
-
+  
   const campoResultado = document.querySelector("#resultado-palavrachave");
 
   const palavrasChave = processaTexto(texto);
 
   campoResultado.textContent = palavrasChave.join(", ");
+
 } 
+
+
 function processaTexto(texto) {
+
   let palavras = texto.split(/\P{L}+/u);
+    
+	for (let i in palavras) {
+	
+    palavras = tiraPalavrasRuins(palavras);
+}
 
- const frequencias = contaFrequencias(palavras);
 
- let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+const frequencias = contaFrequencias(palavras);
 
-  function ordenaPalavra(p1, p2) {
+let ordenadas = Object.keys(frequencias).sort();
+
+    function ordenaPalavra(p1, p2) {
     return frequencias[p2] - frequencias[p1];
   }
+
+  console.log(ordenadas);
 
   return ordenadas.slice(0, 10);
 }
 
+
+
+
 function contaFrequencias(palavras) {
   let frequencias = {};
 
+
   for (let i of palavras) {
-    frequencias[i] = 0;
+     frequencias[i] = 0;
 
     for (let j of palavras) {
       if (i == j) {
@@ -38,20 +60,24 @@ function contaFrequencias(palavras) {
       }
     }
   }
-
   return frequencias;
 }
 
-function tiraPalavrasRuins(palavras) {
-  const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
 
+function tiraPalavrasRuins(palavras) {
   const palavrasBoas = [];
 
   for (let palavra of palavras) {
     if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
-      palavrasBoas.push(palavra);
+       palavrasBoas.push(palavra);
+	   
     }
   }
-
+  
   return palavrasBoas;
+}
+
+
+function ordenaPalavra(p1, p2) {
+  return frequencias[p2] - frequencias[p1];
 } 
